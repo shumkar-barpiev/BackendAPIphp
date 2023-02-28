ba6b7cbb5d402e284e90ec9b9cadabd3736ae00f
shumkar-barpiev <barpievsh@gmail.com>
2023-02-28 20:30:58 +0600

    changing category Entity

--- Models/Entity/CategoryEntity.php
+++ Models/Entity/CategoryEntity.php
@@ -3,11 +3,13 @@
 		private $id;
 		private $categoryName;
 		private $categoryImageName;
+		private $categoryDescription;
 
-		public function __construct($id, $categoryName, $categoryImageName){
+		public function __construct($id, $categoryName, $categoryImageName, $categoryDescription){
 			if($id) $this->id = $id;
 			$this->categoryName = $categoryName;
 			$this->categoryImageName = $categoryImageName;
+			$this->categoryDescription = $categoryDescription;
 		}
     //get functions
     public function getCategoryID(){
@@ -20,6 +22,10 @@
       return $this->categoryImageName;
     }
 
+		public function getcategoryDescription(){
+      return $this->categoryDescription;
+    }
+
     //set functions
     public function setId($id){
 			$this->id = $id;
@@ -29,6 +35,9 @@
 		}
     public function setCategoryImageName($categoryImageName){
 			$this->categoryImageName = $categoryImageName;
+		}
+		public function setcategoryDescription($categoryDescription){
+			$this->categoryDescription = $categoryDescription;
 		}
 	}
 ?>
